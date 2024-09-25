f2c3d59596523e0602028e9a5fcf4f3e42c40e6e
prabinpoudel12 <poudelgang100@gmail.com>
2024-09-25 09:23:23 +0545

    up

--- ECommerceSite-Django/ecommerce/settings.py
+++ ECommerceSite-Django/ecommerce/settings.py
@@ -81,10 +81,15 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecom1',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT':' 3306'  
     }
 }
+
 
 
 # Password validation
